b949ff3a77f262edc776439f8c146b8561bcef3e
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2024-01-25 21:15:23 +0500

    Creating portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
 import About from "@/components/about/page";
 import Navbar from "@/components/header/page";
 import Hero from "@/components/hero/page";
+import WorkExperience from "@/components/workExperience/page";
 
 export default function Home() {
   return (
     <div className="bg-[#035352] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Navbar />
 
-      {/* Hero */}
       <section id="hero" className="snap-start">
         <Hero />
       </section>
-      {/* About */}
       <section id="about" className="snap-center">
-      <About/>
+        <About />
       </section>
-      {/* Exprience */}
-
+      <section className="snap-center" id="experience">
+        <WorkExperience />
+      </section>
       {/* skills */}
 
       {/* Projects */}
--- src/components/about/page.tsx
+++ src/components/about/page.tsx
@@ -6,22 +6,50 @@ type Props = {};
 
 const About = (props: Props) => {
   return (
-    <>
-      <div className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-5xl px-10 justify-evenly mx-auto items-center">
-          <h3 className="absolute top-24 uppercase tracking-[20px] text-white text-2xl">About</h3>
-        <div className="flex mt-[300px]">
-            <motion.img src="/mtahir.jpg" alt="Tahir image" className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]" />
-          <div>
-            <h4>A little About me</h4>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis natus ex, velit numquam rem unde officia cumque quae alias maiores quo odio explicabo voluptas commodi culpa, amet earum voluptatem! Molestiae?
-            Debitis temporibus libero aliquam maxime! Et cumque maiores, nulla deleniti at, vero amet vitae sequi ducimus, error obcaecati similique voluptatibus ad deserunt? Perferendis ex iure aut adipisci eligendi explicabo suscipit.
-            Reprehenderit quos corrupti totam veniam itaque, nam nesciunt ex ipsa assumenda voluptas, id quam molestiae aut quae. Voluptates minima, distinctio necessitatibus commodi odit magnam, iusto natus eveniet et iste animi.
-            Ipsum et animi voluptate corporis possimus nemo porro officia non ea rerum quaerat ipsam adipisci voluptates dicta repellat illo ratione molestias unde mollitia ex expedita architecto, beatae itaque. Eaque, natus?
-            Alias enim voluptatum, asperiores, corporis atque dicta eos modi debitis incidunt labore sit aut autem et accusantium blanditiis suscipit fuga consequatur nobis cupiditate quas. Doloribus, voluptates est? A, tempore harum!</p>
-          </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row  max-w-5xl px-6 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-20 uppercase tracking-[16px] text-white text-2xl">
+        About
+      </h3>
+      <div className="flex flex-col mx-auto items-center h-screen md:flex-row">
+        <motion.img
+          initial={{
+            x: -170,
+            opacity: 0,
+          }}
+          transition={{
+            duration: 1.1,
+          }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          src="/tahir.jpg"
+          className="mb-16 md:mb-0 flex-shrink-0 w-52 h-52 rounded-full object-cover md:rounded-lg md:w-55 md:h-75 xl:w-[300px] xl:h-[370px]"
+        />
+        <div className="space-y-7 px-0 md:px-4">
+          <h4 className="text-3xl font-semibold">
+            Here is a{" "}
+            <span className="underline decoration-[#F7AB0A]">little</span>{" "}
+            background
+          </h4>
+          <p className="text-base">
+            Embarked on a digital odyssey post my Computer Science degree, I
+            honed my craft at a transformative coding boot camp, emerging as a
+            proficient MERN Stack Developer. Expanding my repertoire, I embraced
+            TypeScript and Next.js, elevating my skill set. Today, as a Full
+            Stack Developer, I seamlessly navigate between frontend finesse and
+            backend wizardry, crafting immersive digital experiences. Beyond the
+            syntax, my code tells stories of innovation and user-centric design,
+            ushering in a future where creativity harmonizes with efficiency.
+            Join me on this perpetual quest, as we code the narrative of
+            tomorrow's digital landscapes. 🚀✨
+          </p>
         </div>
       </div>
-    </>
+    </motion.div>
   );
 };
 

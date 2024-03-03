3d825dcd49da23ebedf89e6e6916e9c9a45ce091
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2024-03-03 13:50:34 +0500

    update api version
    updated api  version in.

--- sanity.utils.ts
+++ sanity.utils.ts
@@ -5,7 +5,7 @@ import process from "process"; // Required to access process.env
 const client = createClient({
   projectId: "lf8cn0gf",
   dataset: "production",
-  apiVersion: "2024-02-20"
+  apiVersion: "2024-02-19"
 });
 
 console.log("Created client with configuration:", client); // Log the client object
